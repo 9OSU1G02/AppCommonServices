8b061eaafa01aab401324036214df95a8432ca99
9OSU1G02 <huyquocnguyen1902@gmail.com>
2023-08-25 21:31:59 +0700

    Initial Commit

---
+++ AppCommonServices/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppCommonServices
+//
+//  Created by 9OSU1G02 on 8/25/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
